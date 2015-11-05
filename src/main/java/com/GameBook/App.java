9fa1f5ccb2cabab80cee2ad4993415d8f097410d
Joshua Dalton <shadow.walker.227@gmail.com>
2015-11-04 19:37:02 -0500

    Recreated the project to be a client side application. Basic functionality already implemented to communicate between client and database

---
+++ src/main/java/com/GameBook/App.java
@@ -0,0 +1,15 @@
+package com.GameBook;
+
+/**
+ * Created by Joshua Dalton, Melissa Stricker, and Jake Hatfield on 11/4/2015.
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        //Code goes here
+        TomCatManager TCManager = new TomCatManager();
+
+        TCManager.getData();
+    }
+}
